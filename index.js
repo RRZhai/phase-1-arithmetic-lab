5251e3e48dd1ed870573993152f5553e31e058eb
Shiyao Zhai <zsshiyaozhai@gmail.com>
2023-04-10 18:44:46 -0500

    passed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = Math.floor(Math.random() * 10) + 1;
+let num2 = Math.floor(Math.random() * 10) + 1;
+let multiply = 62;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 1;
+let num4 = 4;
+let mod = num4 * num3;
+
+let max = Math.max(1, 8, 20);
